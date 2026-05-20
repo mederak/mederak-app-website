4e353218f6492c59d9fe86475fde47813f750054
BuildTools <unconfigured@null.spigotmc.org>
2026-05-20 09:04:51 +0200

    Switch importer CTAs to Marketplace flow

--- apps/excel-backlog-importer-for-jira/assets/site-config.js
+++ apps/excel-backlog-importer-for-jira/assets/site-config.js
@@ -2,11 +2,13 @@ window.MEDERAK_PRODUCT_CONFIG = {
   PRODUCT_NAME: "Excel Backlog Importer for Jira",
   PRODUCT_SLUG: "excel-backlog-importer-for-jira",
   SUPPORT_EMAIL: "support.jira@mederak.pl",
-  MARKETPLACE_URL: "#",
-  CTA_URL: "mailto:support.jira@mederak.pl?subject=Excel%20Backlog%20Importer%20early%20access",
+  MARKETPLACE_URL: "https://marketplace.atlassian.com/",
+  CTA_URL: "https://marketplace.atlassian.com/",
+  PRICING_URL: "https://marketplace.atlassian.com/",
   DOCS_URL: "docs.html",
   PRIVACY_URL: "privacy.html",
   SECURITY_URL: "security.html",
   SUPPORT_URL: "support.html",
-  CTA_PRIMARY_LABEL: "Request access"
+  CTA_PRIMARY_LABEL: "Try on Atlassian Marketplace",
+  CTA_PRICING_LABEL: "View pricing"
 };
