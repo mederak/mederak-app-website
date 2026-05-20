a3587c93ae98349f20f7ddc6016eaab8eb977a74
BuildTools <unconfigured@null.spigotmc.org>
2026-05-20 08:54:40 +0200

    Remove demo booking CTA

--- apps/excel-backlog-importer-for-jira/assets/site-config.js
+++ apps/excel-backlog-importer-for-jira/assets/site-config.js
@@ -4,11 +4,9 @@ window.MEDERAK_PRODUCT_CONFIG = {
   SUPPORT_EMAIL: "support.jira@mederak.pl",
   MARKETPLACE_URL: "#",
   CTA_URL: "mailto:support.jira@mederak.pl?subject=Excel%20Backlog%20Importer%20early%20access",
-  DEMO_URL: "mailto:support.jira@mederak.pl?subject=Excel%20Backlog%20Importer%20demo",
   DOCS_URL: "docs.html",
   PRIVACY_URL: "privacy.html",
   SECURITY_URL: "security.html",
   SUPPORT_URL: "support.html",
-  CTA_PRIMARY_LABEL: "Request access",
-  CTA_SECONDARY_LABEL: "Book a demo"
+  CTA_PRIMARY_LABEL: "Request access"
 };
